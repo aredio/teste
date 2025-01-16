eecc1a56c4da872b04ae72b36083553fef0bb352
aredio <aredio.filho@gmail.com>
2025-01-16 18:13:25 -0300

    Initial Commit

---
+++ Questao1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Questao1
+{
+    static void Main(string[] args)
+    {
+        const int Indice = 13; 
+        int soma = 0; 
+        int k = 0;    
+
+        while (k < Indice)
+        {
+            k += 1;
+            soma += k;
+        }
+
+        Console.WriteLine("O valor final de soma é: " + soma);
+    }
+}
